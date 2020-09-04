a0ddc0fb606e0af2e2c59bdc16b098511a75719d
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-09-04 10:31:43 -0300

    Primeiro exercício
    primeiro exercicio da aula de classes do curso es6 - rocketseat

---
+++ main.js
@@ -0,0 +1,16 @@
+class TodoList {
+	constructor(){
+		this.todos = [];
+	}
+
+	addTodo(){
+		this.todos.push('Novo Todo');
+		console.log(this.todos);
+	}
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+	MinhaLista.addTodo();
+}
